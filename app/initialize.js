8c2b600fc03351fb1599d2de4f042d3ef5cf62a8
Fabian Vilers <fabian.vilers@dev-one.com>
2019-12-06 13:35:33 +0100

    Add support for size

--- app/initialize.js
+++ app/initialize.js
@@ -1,8 +1,8 @@
-const POINT_SIZE = 2;
 const LINE_WIDTH = "round";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const color = document.getElementById("color");
+  const color = document.querySelector("#color");
+  const size = document.querySelector("#size");
   const canvas = document.querySelector("#canvas");
   const context = canvas.getContext("2d");
   const points = [];
@@ -11,13 +11,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const redraw = () => {
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
     context.lineJoin = LINE_WIDTH;
-    context.lineWidth = POINT_SIZE;
 
     let previousPoint = null;
 
     for (const point of points) {
       context.beginPath();
       context.strokeStyle = point.color;
+      context.lineWidth = point.size;
 
       if (point.dragging && previousPoint) {
         context.moveTo(previousPoint.x, previousPoint.y);
@@ -45,12 +45,25 @@ document.addEventListener("DOMContentLoaded", () => {
     isPainting = false;
   };
 
+  const updateSize = e => {
+    size.nextSibling.textContent = `${size.value} px`;
+  };
+
+  updateSize();
+  size.addEventListener("input", updateSize);
+
   canvas.addEventListener("mousedown", e => {
     const x = e.pageX - canvas.offsetLeft;
     const y = e.pageY - canvas.offsetTop;
 
     isPainting = true;
-    points.push({ x, y, color: color.value, dragging: false });
+    points.push({
+      x,
+      y,
+      color: color.value,
+      size: size.value,
+      dragging: false
+    });
     redraw();
   });
   canvas.addEventListener("mousemove", e => {
@@ -58,7 +71,13 @@ document.addEventListener("DOMContentLoaded", () => {
       const x = e.pageX - canvas.offsetLeft;
       const y = e.pageY - canvas.offsetTop;
 
-      points.push({ x, y, color: color.value, dragging: true });
+      points.push({
+        x,
+        y,
+        color: color.value,
+        size: size.value,
+        dragging: true
+      });
       redraw();
     }
   });
