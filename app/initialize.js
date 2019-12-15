5f76e125b517273b922d0087f8a297a07610fe3a
Fabian Vilers <fabian.vilers@dev-one.com>
2019-12-15 10:29:13 +0100

    Update drawing with more curves

--- app/initialize.js
+++ app/initialize.js
@@ -1,47 +1,87 @@
-const LINE_WIDTH = 'round';
+const LINE_WIDTH = "round";
 
-document.addEventListener('DOMContentLoaded', () => {
-  const color = document.querySelector('#color');
-  const size = document.querySelector('#size');
-  const clear = document.querySelector('#clear');
-  const canvas = document.querySelector('#canvas');
-  const context = canvas.getContext('2d');
+const copyCanvas = canvas => {
+  const copy = document.createElement("canvas");
+  copy.height = canvas.height;
+  copy.width = canvas.width;
+
+  return copy;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const color = document.querySelector("#color");
+  const size = document.querySelector("#size");
+  const clear = document.querySelector("#clear");
+  const canvas = document.querySelector("#canvas");
+  const memoryCanvas = copyCanvas(canvas);
+  const memoryContext = memoryCanvas.getContext("2d");
+  const context = canvas.getContext("2d");
   const points = [];
   let isPainting = false;
 
+  context.lineJoin = "round";
+  context.lineCap = "round";
+  context.lineWidth = 20;
+
   const redraw = () => {
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-    context.lineJoin = LINE_WIDTH;
+    context.lineJoin = "round";
+    context.lineCap = "round";
+    context.lineWidth = size.value;
 
-    let previousPoint = null;
-
-    for (const point of points) {
-      context.beginPath();
-      context.strokeStyle = point.color;
-      context.lineWidth = point.size;
-
-      if (point.dragging && previousPoint) {
-        context.moveTo(previousPoint.x, previousPoint.y);
-      } else {
-        context.moveTo(point.x - 1, point.y);
-      }
-
-      context.lineTo(point.x, point.y);
-      context.closePath();
-      context.stroke();
-      previousPoint = point;
+    if (points.length === 0) {
+      return;
     }
+
+    // draw a basic circle instead
+    if (points.length < 6) {
+      const point = points[0];
+      context.beginPath(),
+        context.arc(
+          point.x,
+          point.y,
+          context.lineWidth / 2,
+          0,
+          Math.PI * 2,
+          !0
+        ),
+        context.closePath(),
+        context.fill();
+      return;
+    }
+    context.beginPath();
+    context.moveTo(points[0].x, points[0].y);
+
+    // draw a bunch of quadratics, using the average of two points as the control point
+    let i;
+    for (i = 1; i < points.length - 2; i++) {
+      const c = (points[i].x + points[i + 1].x) / 2,
+        d = (points[i].y + points[i + 1].y) / 2;
+      context.quadraticCurveTo(points[i].x, points[i].y, c, d);
+    }
+    context.quadraticCurveTo(
+      points[i].x,
+      points[i].y,
+      points[i + 1].x,
+      points[i + 1].y
+    );
+    context.stroke();
   };
 
   const clearCanvas = () => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    memoryContext.clearRect(0, 0, memoryCanvas.width, memoryCanvas.height);
     points.length = 0;
-    window.requestAnimationFrame(redraw);
   };
 
   const resizeCanvas = () => {
     canvas.height = canvas.clientHeight - 1; // -1 to prevent scrollbars
     canvas.width = canvas.clientWidth;
-    window.requestAnimationFrame(redraw);
+    memoryCanvas.height = canvas.height;
+    memoryCanvas.width = canvas.width;
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(memoryCanvas, 0, 0);
+    redraw();
   };
 
   const updateSize = e => {
@@ -65,26 +105,32 @@ document.addEventListener('DOMContentLoaded', () => {
     if (e.button === undefined || e.button === 0) {
       isPainting = true;
       points.push(createPoint(e, false));
-      window.requestAnimationFrame(redraw);
     }
   };
 
   const onDrag = e => {
-    e.preventDefault();
-
     if (isPainting) {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(memoryCanvas, 0, 0);
       points.push(createPoint(e, true));
-      window.requestAnimationFrame(redraw);
+      redraw();
     }
   };
 
   const onRelease = () => {
-    isPainting = false;
-    window.requestAnimationFrame(redraw);
+    if (isPainting) {
+      isPainting = false;
+      memoryContext.clearRect(0, 0, memoryCanvas.width, memoryCanvas.height);
+      memoryContext.drawImage(canvas, 0, 0);
+      points.length = 0;
+    }
   };
 
   const onCancel = () => {
     isPainting = false;
+    memoryContext.clearRect(0, 0, memoryCanvas.width, memoryCanvas.height);
+    memoryContext.drawImage(canvas, 0, 0);
+    points.length = 0;
   };
 
   // Init
@@ -92,17 +138,17 @@ document.addEventListener('DOMContentLoaded', () => {
   updateSize();
 
   // Event listeners
-  window.addEventListener('resize', resizeCanvas);
-  size.addEventListener('input', updateSize);
-  clear.addEventListener('click', clearCanvas);
+  window.addEventListener("resize", resizeCanvas);
+  size.addEventListener("input", updateSize);
+  clear.addEventListener("click", clearCanvas);
 
-  canvas.addEventListener('mousedown', onPress, false);
-  canvas.addEventListener('mousemove', onDrag, false);
-  canvas.addEventListener('mouseup', onRelease, false);
-  canvas.addEventListener('mouseout', onCancel, false);
+  canvas.addEventListener("mousedown", onPress, false);
+  canvas.addEventListener("mousemove", onDrag, false);
+  canvas.addEventListener("mouseup", onRelease, false);
+  // canvas.addEventListener("mouseout", onCancel, false);
 
-  canvas.addEventListener('touchstart', onPress, false);
-  canvas.addEventListener('touchmove', onDrag, false);
-  canvas.addEventListener('touchend', onRelease, false);
-  canvas.addEventListener('touchcancel', onCancel, false);
+  canvas.addEventListener("touchstart", onPress, false);
+  canvas.addEventListener("touchmove", onDrag, false);
+  canvas.addEventListener("touchend", onRelease, false);
+  canvas.addEventListener("touchcancel", onCancel, false);
 });
